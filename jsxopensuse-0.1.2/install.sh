58e4dbc7c8d65541cf677108a0537634b9f7051f
jsx <jean-sebastien.herbaux@epitech.eu>
2017-02-18 04:42:22 +0100

    Handle package list / add new command

--- jsxopensuse-0.1.2/install.sh
+++ jsxopensuse-0.1.2/install.sh
@@ -6,15 +6,16 @@ rm -rf ~/.jsxopensuse
 mkdir ~/.jsxopensuse
 echo "--DONE--"
 echo "--STEP (2 / 5)-- Moving all utils into directory [Dockerfile, runner.sh, OpenSuse.Tumbleweed] => [~/.jsxopensuse]"
-cp ./Dockerfile ./runner.sh ./openSUSE-Tumbleweed.tar.xz ./openSUSE-Tumbleweed.tar.xz.sha256 -t ~/.jsxopensuse
+cp ./Dockerfile ./runner.sh ./jsxos_packages ./packages -t ~/.jsxopensuse
 echo "--DONE--"
 echo "--STEP (4 / 5)-- Building docker image on this computer as \"jsxopensuse\""
 docker rmi -f `docker inspect jsxopensuse | grep "Id" | cut -c 23-34`
-sudo docker build -t jsxopensuse ~/.jsxopensuse/
+sudo docker build --build-arg packages="`cat ~/.jsxopensuse/packages`" --tag=jsxopensuse ~/.jsxopensuse/
 echo "--DONE--"
 echo "--STEP (5 / 5)-- Move executables running and update \"jsxopensuse\""
 cp ./jsxos_run /usr/local/bin/jsxos_run
 cp ./jsxos_update /usr/local/bin/jsxos_update
+cp ./jsxos_packages /usr/local/bin/jsxos_packages
 echo "--DONE--"
 echo "Use jsxos_run [BINARY_NAME] to launch the compilation and the execution of your current directory and jsxos_update to update your docker with the new runner.sh."
 echo "Be careful, you must be root for all the operations."
