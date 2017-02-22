96550b5076b3faf77f8f0cbd350c844d1f310f48
AntoDury <antoinedury.pro@gmail.com>
2017-02-22 10:14:40 +0100

    Update install.sh
    Need to run docker and executable copy as sudo

--- jsxopensuse-0.1.2/install.sh
+++ jsxopensuse-0.1.2/install.sh
@@ -9,13 +9,13 @@ echo "--STEP (2 / 5)-- Moving all utils into directory [Dockerfile, runner.sh, O
 cp ./Dockerfile ./runner.sh ./jsxos_packages ./packages -t ~/.jsxopensuse
 echo "--DONE--"
 echo "--STEP (4 / 5)-- Building docker image on this computer as \"jsxopensuse\""
-docker rmi -f `docker inspect jsxopensuse | grep "Id" | cut -c 23-34`
+sudo docker rmi -f `sudo docker inspect jsxopensuse | grep "Id" | cut -c 23-34`
 sudo docker build --build-arg packages="`cat ~/.jsxopensuse/packages`" --tag=jsxopensuse ~/.jsxopensuse/
 echo "--DONE--"
 echo "--STEP (5 / 5)-- Move executables running and update \"jsxopensuse\""
-cp ./jsxos_run /usr/local/bin/jsxos_run
-cp ./jsxos_update /usr/local/bin/jsxos_update
-cp ./jsxos_packages /usr/local/bin/jsxos_packages
+sudo cp ./jsxos_run /usr/local/bin/jsxos_run
+sudo cp ./jsxos_update /usr/local/bin/jsxos_update
+sudo cp ./jsxos_packages /usr/local/bin/jsxos_packages
 echo "--DONE--"
 echo "Use jsxos_run [BINARY_NAME] to launch the compilation and the execution of your current directory and jsxos_update to update your docker with the new runner.sh."
 echo "Be careful, you must be root for all the operations."
