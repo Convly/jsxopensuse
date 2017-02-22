275ea68dbd3374cccbe4ab274a8124223a24587b
jsx <jean-sebastien.herbaux@epitech.eu>
2017-02-22 15:04:42 +0100

    Fix update script to avoid bad rebuild

--- jsxopensuse-0.1.2/install.sh
+++ jsxopensuse-0.1.2/install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "--START--"
-echo "--STEP (1 / 5)-- Creating directory for docker [~/.jsxopensuse]"
+echo "--STEP (1 / 4)-- Creating directory for docker [~/.jsxopensuse]"
 rm -rf ~/.jsxopensuse
 mkdir ~/.jsxopensuse
 echo "--DONE--"
-echo "--STEP (2 / 5)-- Moving all utils into directory [Dockerfile, runner.sh, OpenSuse.Tumbleweed] => [~/.jsxopensuse]"
+echo "--STEP (2 / 4)-- Moving all utils into directory [Dockerfile, runner.sh, OpenSuse.Tumbleweed] => [~/.jsxopensuse]"
 cp ./Dockerfile ./runner.sh ./jsxos_packages ./packages -t ~/.jsxopensuse
 echo "--DONE--"
-echo "--STEP (4 / 5)-- Building docker image on this computer as \"jsxopensuse\""
+echo "--STEP (3 / 4)-- Building docker image on this computer as \"jsxopensuse\""
 sudo docker rmi -f `sudo docker inspect jsxopensuse | grep "Id" | cut -c 23-34`
 sudo docker build --build-arg packages="`cat ~/.jsxopensuse/packages`" --tag=jsxopensuse ~/.jsxopensuse/
 echo "--DONE--"
-echo "--STEP (5 / 5)-- Move executables running and update \"jsxopensuse\""
+echo "--STEP (4 / 4)-- Move executables running and update \"jsxopensuse\""
 sudo cp ./jsxos_run /usr/local/bin/jsxos_run
 sudo cp ./jsxos_update /usr/local/bin/jsxos_update
 sudo cp ./jsxos_packages /usr/local/bin/jsxos_packages
